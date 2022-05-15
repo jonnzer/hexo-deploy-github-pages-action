aa795feeeaf1f669749edc70fb38afb9a27ff16d
夜曲 <janebenjohn@outlook.com>
2022-05-15 20:19:07 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -51,6 +51,7 @@ npm cache verify
 echo ">>> change npm config ..."
 npm config set registry https://registry.npmjs.org/
 npm install -g npm@7.24.2
+npm audit fix
 
 echo ">>> Install NPM dependencies ..."
 
