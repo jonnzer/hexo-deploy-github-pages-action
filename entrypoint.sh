9e2f37796a61029cbacd90d4d7c764f27c5cab8b
夜曲 <janebenjohn@outlook.com>
2022-05-15 20:03:23 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,7 +45,7 @@ del_package="./node_modules"
 
 echo ">>> npm cache clear ..."
 rm -rf "${del_package}"
-npm cache clear
+npm cache clear --force
 
 echo ">>> Install NPM dependencies ..."
 npm install
